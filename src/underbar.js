c5092883043d9b462cf262acfbe70f818fa6790d
Siddharth Sukumar <siddharth@inauth.com>
2015-03-16 21:15:32 -0700

    Added code for the methods _.map, _.reduce and _.filter

--- src/underbar.js
+++ src/underbar.js
@@ -85,6 +85,16 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+     var answerArray = [];
+     
+     each (collection, function (value) {
+            
+            if (test(value)){
+             answerArray.push (value);
+            }
+     
+     });
+     return answerArray;
   };
 
   // Return all elements of an array that don't pass a truth test.
@@ -103,6 +113,13 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    
+    var anwerArray = [];
+    
+    each (collection, function (value, index, list) {
+     anwerArray.push (iterator (value, index, list));
+   });
+    return anwerArray;
   };
 
   /*
@@ -144,6 +161,15 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+   if (collection === null) return collection;
+   var accumulator;
+   
+   each (collection, function (value, index ,list) {
+      
+   accumulator =  iterator ( collection, value, index, list);
+       
+  });
+   return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
